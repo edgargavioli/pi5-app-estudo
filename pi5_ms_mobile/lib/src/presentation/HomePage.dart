4936ef93eae9df693a2ba2cab3886cd5bd0c1e4a
edgar <edgargavioli14@gmail.com>
2025-04-10 18:21:42 -0300

    feat: altera modo em que o tema é aplicado

--- pi5_ms_mobile/lib/src/presentation/HomePage.dart
+++ pi5_ms_mobile/lib/src/presentation/HomePage.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('You have pushed the button this many times:'),
+          children: <Widget> [
+            const Text(
+              "Você apertou o botão esta quantidade de vezes:",
+            ),
             Text(
-              '0', // Exemplo fixo
-              style: TextStyle(fontSize: 32),
+              "$_counter",
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // ação do botão
-        },
-        tooltip: 'Increment',
+        onPressed: _incrementCounter,
+        tooltip: "Adicionar",
         child: const Icon(Icons.add),
       ),
     );
